97620fa0252e5d8f31ed3215ea2ce99aefb9c05b
Jabareen <n.jabareen@tum.de>
2020-03-31 21:13:42 +0200

    Add pathlib, remove os

--- training/export_all_models.py
+++ training/export_all_models.py
@@ -2,85 +2,54 @@
 Export all models in a given directory.
 """
 
-import os
-from os.path import join
+from pathlib import Path
 import subprocess
 import argparse
-import re
 
 
 def _dir_path(string):
-    if os.path.isdir(string):
-        return string
-    else:
-        raise NotADirectoryError(string)
+    path = Path(string)
+    if path.is_dir():
+        return path
+    raise NotADirectoryError(string)
+
+
+def find_latest_model(path):
+    path = Path(path)
+    checkpoints = [str(x).split("ckpt-")[1].split(".")[0] for x in path.glob(r"*ckpt*")]
+    checkpoints = [int(x) for x in checkpoints]
+    return max(checkpoints)
 
 
 def main(model_dir, out_dir):
 
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir)
+    out_dir.mkdir(exist_ok=True)
+    model_subdirs = [x for x in model_dir.iterdir() if x.is_dir()]
 
-    for model in os.listdir(model_dir):
+    for model in model_subdirs:
 
-        max_model_number = 0
-        config_file_name = None
+        max_model_number = find_latest_model(model)
+        config_path = next(model.glob("[!pipeline]*.config"))
 
-        # Find latest model and config file
-        for input_file in os.listdir(join(model_dir, model)):
-
-            if ".config" in input_file and "pipeline" not in input_file:
-                config_file_name = input_file
-                continue
-
-            if "model.ckpt-" in input_file:
-                numbers = re.findall(r'\d+', input_file)
-                if len(numbers) < 1:
-                    continue
-
-                if int(numbers[0]) > max_model_number:
-                    max_model_number = int(numbers[0])
-
-        if max_model_number == 0 or not config_file_name:
-            print(f"\nWARNING: Could not find latest model in {model_dir}{model}")
-            continue
-
-        latest_model_path = join(model_dir, model, f"model.ckpt-{max_model_number}")
-        config_path = join(model_dir, model, config_file_name)
-        model_out_dir = join(out_dir, model)
-
-        try:
-            os.mkdir(model_out_dir)
-        except:
-            res = None
-            while not res:
-                res = input(f"\nOutput directory {model_out_dir} already exists. Do you want to override it? [y/n]")
-                if res == "y":
-                    os.rmdir(model_out_dir)
-                    os.mkdir(model_out_dir)
-                elif res == "n":
-                    raise Exception()
-                else:
-                    print("Answer with \'y\' or \'n\' !")
-                    res = None
+        model = model.joinpath(f"model.ckpt-{max_model_number}")
+        model_out_dir = out_dir.joinpath(model.name)
 
         subprocess.run(["python", "export_inference_graph.py", "--input_type image_tensor",
                         "--pipeline_config_path", config_path,
-                        "--trained_checkpoint_prefix", latest_model_path,
-                        "--output_directory", model_out_dir],
-                        check=True)
+                        "--trained_checkpoint_prefix", model,
+                        "--output_directory", model_out_dir], check=True)
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description='Export all model from a given directory.')
+        description='Export all models from a given directory.')
 
-    parser.add_argument('--model_dir', type=_dir_path,
+    parser.add_argument("--model_dir", "-m", type=_dir_path,
                         help="Path to directory with folders models.")
-    parser.add_argument('--out_dir',
+    parser.add_argument("--out_dir", "-o",
                         help="Path to directory to save all models.")
 
     args = parser.parse_args()
 
-    main(args.model_dir, args.out_dir)
+    main(Path(args.model_dir), Path(args.out_dir))
--- training/train_all_models.py
+++ training/train_all_models.py
@@ -5,51 +5,50 @@ Each model folder must contain a config file with naming convention: {model_name
 """
 
 import argparse
-import os
-from os.path import join
-from glob import glob
+from pathlib import Path
 import subprocess
 import time
 from datetime import datetime
 
+
 def main(train_path, log_name):
 
-    MODELS = os.listdir(train_path)
+    models = [x for x in train_path.iterdir() if x.is_dir()]
 
-    if os.path.isfile(LOG_FILENAME):
-        os.remove(LOG_FILENAME)
+    if log_name.is_file():
+        log_name.unlink()
 
-    for model in MODELS:
-        model_path = join(train_path, model)
-
-        if not os.path.isdir(model_path):
-            continue
+    for model in models:
 
         with open(log_name, 'a+') as log_file:
             log_file.write(f"Start training {model} {datetime.now()}\n")
 
-        config_path = glob(join(model_path, "[!pipeline]*.config"))[0]
+        config_path = next(model.glob("[!pipeline]*.config"))
 
         tic = time.time()
         subprocess.run(["python", "train.py", "--logtostderr",
-                        f"--train_dir={model_path}", f"--pipeline_config_path={config_path}"])
+                        f"--train_dir={model}", f"--pipeline_config_path={config_path}"])
         tac = time.time()
 
-        runtime = round(int(tac - tic)/60)
-
         with open(log_name, 'a') as log_file:
-            log_file.write(f"FINISHED in {runtime} minutes\n")
+            log_file.write(f"FINISHED in {round(int(tac - tic)/60)} minutes\n")
+
+
+def _text_file(path):
+    if path.endswith(".txt"):
+        return Path(path)
+    raise FileNotFoundError("Log file must be a text file ending with .txt!")
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument("--config_dir", "-c", required=True)
-    parser.add_argument("--log_path", "-l")
+    parser.add_argument("--log_path", "-l", type=_text_file)
 
     args = parser.parse_args()
 
-    CONFIG_DIR = args.config_dir
-    LOG_FILENAME = args.log_path or "train_log.txt"
+    CONFIG_DIR = Path(args.config_dir)
+    LOG_FILENAME = args.log_path if args.log_path else CONFIG_DIR.joinpath("train_log.txt")
 
-    main(CONFIG_DIR, LOG_FILENAME)
+    main(CONFIG_DIR, Path(LOG_FILENAME))
