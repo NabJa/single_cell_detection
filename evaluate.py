b5fc59cb0a8186dcf79ace53808a5187ca593c90
Jabareen <n.jabareen@tum.de>
2020-04-01 20:52:44 +0200

    Refactor evaluation

--- evaluate.py
+++ evaluate.py
@@ -1,62 +1,131 @@
 """
 Script to evaluate a trained model.
+
+Evaluation metrics:
+    - Prediction images (Single images)
+    - Precision recall values (One per image)
+    - TODO AUC boxplot (One box for every model)
+    - TODO Tracks based on TrackMate (One XML file for every model)
 """
 
-import os
-from os.path import join, isdir, basename, dirname
-from glob import glob
+from pathlib import Path
 from time import time
-from datetime import datetime
+from sklearn.metrics import auc
+import pickle
+
 import argparse
 import cv2
 from tqdm import tqdm
-import prediction.prediction_utils as predict
-import prediction.predict_on_series as series_pred
+
+from prediction.prediction_utils import load_model, validation_predictor
 from data import bbox_utils as box
-from training import export_all_models
+from data import tf_record_loading as loader
 import visualization
 import statistics
 
-
-def main(graph_dir, image_path, out_dir):
-
-    assert isdir(graph_dir), "Model dir not a directory."
-
-    eval_images = glob(join(image_path, "*.png"))
-
-    _, _, out_dir_images = init_subdirs(out_dir, basename(graph_dir))
-
-    for graph in glob(join(graph_dir, "*")):
-        print(f"Evaluating {basename(graph)}")
-
-        model = predict.load_model(graph)
-        predictor = series_pred.predictor(model, eval_images)
-
-        for i, (prediction, image) in enumerate(tqdm(predictor, total=len(eval_images))):
-            bboxes = prediction.get("detection_boxes")[prediction.get("detection_scores") >= 0.5]
-            write_image_prediction(join(out_dir_images, f"prediction_{i}.png"), image, bboxes)
-            #TODO save roc-curves
+METRICS = {
+    "aucs": [],
+    "maps": [],
+    "precisions": [],
+    "recalls": [],
+    "images": []
+}
 
 
-def init_subdirs(out_dir, graph_name):
+def main(graph_dir):
+
+    evaluation_path = make_eval_dir(graph_dir)
+    model_dirs = [x for x in graph_dir.iterdir() if x.joinpath("saved_model").is_dir()]
+
+    for model_dir in model_dirs:
+        print("Evaluating model: ", model_dir.name)
+        model_eval_dir = evaluation_path.joinpath(model_dir.name)
+        model_eval_dir.mkdir()
+        model = load_model(model_dir)
+
+        validation_path = find_validation_image_path(model_dir)
+        validation_length = find_validation_length(validation_path)
+        predictor = validation_predictor(model, validation_path)
+
+        metrics = evaluate(predictor, METRICS, validation_length)
+
+        save_metrics(metrics, model_eval_dir)
+
+
+def save_metrics(metrics, out_dir):
     """
-    Init folder structure:
-        evaluation
-            -> model
-                -> images
+    Save images in metrics as images and other values in pickle file.
     """
-    evaluation_path = join(out_dir, "evaluation")
-    make_new_dir(evaluation_path)
 
-    # Directory for every model
-    model_path = join(evaluation_path, graph_name)
-    make_new_dir(model_path)
+    out_dir = Path(out_dir)
 
-    # Image directory in model
-    out_image_path = join(model_path, "images")
-    make_new_dir(out_image_path)
+    image_dir = out_dir.joinpath("images")
+    image_dir.mkdir()
+    for i, img in enumerate(metrics.pop("images", [])):
+        img_path = str(image_dir.joinpath(f"image_{i}.png"))
+        cv2.imwrite(img_path, img)
 
-    return evaluation_path, model_path, out_image_path
+    pickle.dump(out_dir.joinpath("metrics.p").open("wb"), metrics)
+
+
+def evaluate(predictor, metric, total=None, detection_score=0.5):
+    """
+    Evaluates prediction of predictor.
+
+    :param predictor: Generator of predictions.
+    :param metric: Dictionary of metrics to be saved.
+    :param total: Number of validation images.
+    :param detection_score: Classification cutoff.
+    """
+
+    for prediction in tqdm(predictor, total=total):
+
+        image, gt_bboxes, pred = prediction.get("image"), prediction.get("gt_boxes"), prediction.get("pred")
+        pred_bboxes = pred.get("detection_boxes")[pred.get("detection_scores") >= detection_score]
+
+        mAP, precision, recall, _ = statistics.compute_ap(pred_bboxes, gt_bboxes)
+
+        metric.get("aucs").append(auc(recall, precision))
+        metric.get("maps").append(mAP)
+        metric.get("precision", precision)
+        metric.get("recall", recall)
+
+        gt_points = box.boxes_to_center_points(gt_bboxes)
+        pred_points = box.boxes_to_center_points(pred_bboxes)
+        image = visualization.draw_circles_on_image(image, gt_points, pred_points)
+        metric.get("images").append(image)
+
+    return metric
+
+
+def find_validation_image_path(path):
+    """
+    Finds the last input_path field in the pipeline.config file.
+    Last input_path must correspond to evaluation path.
+    """
+    path = Path(path)
+    pipeline_config = next(path.glob("pipeline.config"))
+    res = None
+    with pipeline_config.open() as pipeline:
+        for line in pipeline.readlines():
+            if line.find("input_path") != -1:
+                res = line.split(":", 1)[1].strip()
+    return Path(res[1:-1])
+
+
+def find_validation_length(path):
+    g = loader.tf_dataset_generator(str(path))
+    return len([_ for _ in g])
+
+
+def make_eval_dir(path):
+    try:
+        evaluation_path = path.joinpath("Evaluation")
+        evaluation_path.mkdir()
+    except FileExistsError:
+        evaluation_path = path.joinpath(f"Evaluation{time()}")
+        evaluation_path.mkdir()
+    return evaluation_path
 
 
 def write_image_prediction(path, image, prediction, points=True):
@@ -72,27 +141,11 @@ def write_image_prediction(path, image, prediction, points=True):
     cv2.imwrite(path, image_prediction)
 
 
-def make_new_dir(path):
-    """
-    Make a new directory. If it does already exist, add timestamp.
-    """
-    try:
-        os.mkdir(path)
-    except FileExistsError:
-        dir_name = dirname(path)
-        new_dir = basename(path)
-        time_stamp = datetime.fromtimestamp(time()).strftime("%Y-%m-%d_%H-%M-%S")
-        new_dir = f"{new_dir}-{time_stamp}"
-        os.mkdir(join(dir_name, new_dir))
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-
-    parser.add_argument("--eval_images", "-i", required=True)
-    parser.add_argument("--out_dir", "-o")
-    parser.add_argument("--graph_dirs", "-gd")
-
+    parser.add_argument("--graph_dir", "-g", required=True)
+    parser.add_argument("--metric", "-m", nargs="*", default=["auc", "map", "precision", "recall", "images"])
     args = parser.parse_args()
 
-    main(args.graph_dirs, args.eval_images, args.out_dir)
+    arg_metrics = {k: [] for k in args.metric}
+    main(Path(args.graph_dir))
