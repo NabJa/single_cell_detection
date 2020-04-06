0213d09f20aa9c1ed837bbfab8df598e11bafb79
Jabareen <n.jabareen@tum.de>
2020-04-06 20:41:53 +0200

    Add soft_nms
    Defined in: https://arxiv.org/pdf/1704.04503.pdf

--- procsessing/nms.py
+++ procsessing/nms.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 
-def greedy_nms(dets, scores, thresh):
-    x1 = dets[:, 0]
-    y1 = dets[:, 1]
-    x2 = dets[:, 2]
-    y2 = dets[:, 3]
+def greedy_nms(predictions, scores, thresh):
+    x1 = predictions[:, 0]
+    y1 = predictions[:, 1]
+    x2 = predictions[:, 2]
+    y2 = predictions[:, 3]
 
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
     order = scores.argsort()[::-1]
@@ -22,9 +22,42 @@ def greedy_nms(dets, scores, thresh):
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
         inter = w * h
-        ovr = inter / (areas[i] + areas[order[1:]] - inter)
+        iou = inter / (areas[i] + areas[order[1:]] - inter)
 
-        inds = np.where(ovr <= thresh)[0]
+        inds = np.where(iou <= thresh)[0]
         order = order[inds + 1]
 
     return keep
+
+
+def soft_nms(predictions, scores, thresh, sigma=0.3):
+    x1 = predictions[:, 0]
+    y1 = predictions[:, 1]
+    x2 = predictions[:, 2]
+    y2 = predictions[:, 3]
+
+    areas = (x2 - x1 + 1) * (y2 - y1 + 1)
+    order = scores.argsort()[::-1]
+
+    new_scores = scores.copy()
+    while order.size > 0:
+        i = order[0]
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+        iou = inter / (areas[i] + areas[order[1:]] - inter)
+
+        inds = np.where(iou <= thresh)[0]
+        iou = iou[inds]
+
+        weight = np.exp(-(iou * iou) / sigma)
+
+        new_scores[inds] = scores[inds] * weight
+        order = order[inds + 1]
+
+    return new_scores
