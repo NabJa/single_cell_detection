b864f762be180a32d686e3dfdbb2a597305bbac8
Jabareen <n.jabareen@tum.de>
2020-07-08 13:55:49 +0200

    Refactor tf record writer

--- data/tf_record_writer.py
+++ data/tf_record_writer.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 import cv2
 import tensorflow as tf
+from bbox_utils import point_to_box
 
 from trackmate_xml_to_csv import extract_points_from_trackmate_xml
 
@@ -44,10 +45,10 @@ def is_image(file):
         print("WARNING: unknown image file in image folder: ", file)
 
 
-def write_tf_records(image_dir, bboxes, filename="bboxes.tfrecord"):
+def write_tf_records_from_img_dir(image_dir, bboxes, filename="bboxes.tfrecord"):
     """
     images: paths to image
-    bboxes: list of bboxes in (x1, y1, y2, y2) format for every image
+    bboxes: list of bboxes in (x1, y1, x2, y2) format for every image
     """
 
     filename = join(image_dir, filename)
@@ -57,15 +58,25 @@ def write_tf_records(image_dir, bboxes, filename="bboxes.tfrecord"):
     if len(images) != len(bboxes):
         print("WARNING: Images {} and bboxes {} not same length".format(len(images), len(bboxes)))
 
+    write_tf_record(images, bboxes, filename)
+
+
+def write_tf_record(images, bboxes, filename):
+    """
+    Write tf record to filename.
+
+    :param images: List of image paths
+    :param bboxes: List of bboxes in format (xmin, ymin, xmax, ymax)
+    :param filename: Name of tf record file
+    :return: None
+    """
+
     with tf.io.TFRecordWriter(filename) as writer:
 
-        for i, image_path in enumerate(images):
-            bbox = bboxes[i]
-            image = cv2.imread(image_path, 0)
-
+        for i, (image_path, bbox) in enumerate(zip(images, bboxes)):
+            image = cv2.imread(str(image_path), cv2.IMREAD_UNCHANGED)
             tf_example = bbox_to_tf_example(image, image_path, bbox)
             writer.write(tf_example.SerializeToString())
-
     print("Created tf-record file in: \n", filename)
 
 
@@ -74,6 +85,7 @@ def bbox_to_tf_example(image, filename, bboxes):
     # Parse image metas
     height, width, *_ = image.shape
     image_ext = ".png"
+    filename = str(filename)
 
     encoded_image = cv2.imencode(image_ext, image)[1].tostring()
 
@@ -107,7 +119,7 @@ def bbox_to_tf_example(image, filename, bboxes):
     return tf_example
 
 
-def main(image_dir, annotations, annot_type, bbox_size=50):
+def main(image_dir, annotations, image_pattern, annot_type, bbox_size=40):
 
     if annot_type.lower() == "xml":
         annot_df = extract_points_from_trackmate_xml(annotations, bbox_size)
@@ -121,12 +133,27 @@ def main(image_dir, annotations, annot_type, bbox_size=50):
         bboxes = [list(annot_df[annot_df.Frame == i].bbox)
                   for i in range(len(np.unique(annot_df.Frame)))]
 
-    elif annot_type.lower() == "masks":
-        pass
-    else:
-        print("Unsupported format {}".format(annot_type))
+    elif annot_type.lower() == "resnet23":
+        root_dir, coordinate_pattern = Path(image_dir), annotations
 
-    write_tf_records(image_dir, bboxes)
+        count = len(list(root_dir.rglob(coordinate_pattern)))
+
+        bboxes, images = [], []
+
+        for i, coordinates in enumerate(root_dir.rglob(coordinate_pattern)):
+            if i % 100 == 0:
+                print(f"Parsing annotation {i}/{count}")
+            points = np.load(str(coordinates))
+            boxes = [point_to_box(p, bbox_size) for p in points]
+            bboxes.append(boxes)
+            images.append(coordinates.parent.joinpath(image_pattern))
+
+        write_tf_record(images, bboxes, str(root_dir.joinpath("annotation.tfrecord")))
+        return
+    else:
+        raise NotImplementedError(f"Unsupported format {annot_type}")
+
+    write_tf_records_from_img_dir(image_dir, bboxes)
 
 
 if __name__ == "__main__":
@@ -134,9 +161,11 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create TFrecords from given input format.')
 
     parser.add_argument('--image_dir', '-i', help="Path to directory with images.")
-    parser.add_argument('--annotations', '-a', help="Path to file containing annotations")
-    parser.add_argument('--format', '-f', help="Options: XML | CSV | MASKS")
+    parser.add_argument('--annotations', '-a', help="Path or glob pattern of file containing annotations."
+                                                    "In case of resnet23: pattern of numpy coordinates file")
+    parser.add_argument('--image_pattern', '-ip', help="Glob pattern of images.")
+    parser.add_argument('--format', '-f', help="Options: XML | CSV | resnet23")
 
     args = parser.parse_args()
 
-    main(args.image_dir, args.annotations, args.format)
+    main(args.image_dir, args.annotations, args.image_pattern, args.format)
